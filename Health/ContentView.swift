4da7881f9dff709a9ee69f1e70ba6c9fc26bdab8
Michel Andre Pellegrin Quiroz <michelpellegrin@10-20-56-124.0200.fed.it.iosda.org>
2023-11-14 16:46:15 +0100

    Initial commit

---
+++ Health/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Health
+//
+//  Created by Michel Andre Pellegrin Quiroz on 14/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Health/HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthApp.swift
+//  Health
+//
+//  Created by Michel Andre Pellegrin Quiroz on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
